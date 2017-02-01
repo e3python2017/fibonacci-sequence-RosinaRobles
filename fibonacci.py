a5da5f967a12f2e11cddb1c993f89cbaa4632b06
Rosina Robles <rosina.robles@e3Civichigh.com>
2017-02-01 11:34:25 -0800

    first programing assignment
    code

--- fibonacci.py
+++ fibonacci.py
@@ -3,19 +3,19 @@ def fibonacci(n):
     Return a list containing the Fibonacci series up to n."""
     result = []
 
-    # 1. Create a variable named 'a' and set it to 1
-    # a = 1
+     1.Create a variable named'a'and set it to 1
+     a = 1
 
-    # 2. Create a variable named 'b' and set it to 1
+     2. Create a variable named 'b' and set it to 1
 
     while a < n:
         result.append(a)
 
-        # 3. set a variable 'tmp_var' equal to 'b'
+         3. set a variable 'tmp_var' equal to 'b'
 
-        # 4. set a variable 'b' equal to the sum of 'b' and 'a'
+         4. set a variable 'b' equal to the sum of 'b' and 'a'
 
-        # 5. set the variable 'a' to 'tmp_var'
+         5. set the variable 'a' to 'tmp_var'
     return result
 
 print(fibonacci(100))
